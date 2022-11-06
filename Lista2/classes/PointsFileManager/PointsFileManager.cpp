8b5b37bf79a1ca0d1572645854d5708cdf8af275
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-11-07 00:49:19 +0100

    Finished points file manager for 2D points

--- Lista2/classes/PointsFileManager/PointsFileManager.cpp
+++ Lista2/classes/PointsFileManager/PointsFileManager.cpp
@@ -8,20 +8,88 @@ PointsFileManager::PointsFileManager(std::string pathToFile) {
 
 // Delete unused things
 PointsFileManager::~PointsFileManager() {
+    dataFile->close();
     delete dataFile;
 }
 
 // Return file number of lines
 int PointsFileManager::getSize() {
-    return 5;
+    clearFlag();
+    int count = 0;
+    std::string line;
+    while (dataFile->peek() != EOF) {
+        std::getline(*dataFile, line);
+        count++;
+    }
+
+    return count;
 }
 
 // Return filename
 std::string PointsFileManager::getFilename() {
-    return filename;
+    std::string preparedFilename = "";
+    int lastSlash = 0;
+
+    for (int i = 0; i < path.length(); i++) {
+        if (path[i] == '/' || path[i] == '\\') {
+            lastSlash = i;
+        }
+    }
+
+    return path.substr(lastSlash + 1, path.length() - 1);
 }
 
 // Return path to file
 std::string PointsFileManager::getPath() {
     return path;
+}
+
+// Create 2D points and save in array
+void PointsFileManager::createPoints(Point2D *pointsArray, int size) {
+    clearFlag();
+    std::string line;
+    int i = 0;
+    while (std::getline(*dataFile, line)) {
+        Point2D newPoint = create2dPoint(line);
+        pointsArray[i] = newPoint;
+        i++;
+    }
+}
+
+// Create 3D points and save in array
+void PointsFileManager::createPoints(Point3D *pointsArray, int size) {
+    clearFlag();
+    std::string line;
+    int i = 0;
+    while (std::getline(*dataFile, line)) {
+        Point3D newPoint = create3dPoint(line);
+        pointsArray[i] = newPoint;
+        i++;
+    }
+}
+
+// Clear ifstream flag
+void PointsFileManager::clearFlag() {
+    dataFile->clear();
+    dataFile->seekg(0, std::ios::beg);
+}
+
+// Create 2D point based on string line from file
+Point2D PointsFileManager::create2dPoint(std::string line) {
+    int semiIndex = 0;
+    for (int i = 0; i < line.length(); i++) {
+        if (line[i] == ';') {
+            semiIndex = i;
+            break;
+        }
+    }
+
+    float x = std::stof(line.substr(0, semiIndex));
+    float y = std::stof(line.substr(semiIndex + 1, line.length() - 1));
+    return Point2D(x, y);
+}
+
+// Create 3D point based on string line from file
+Point3D PointsFileManager::create3dPoint(std::string line) {
+    return Point3D(0, 0, 0);
 }
--- Lista2/classes/PointsFileManager/PointsFileManager.h
+++ Lista2/classes/PointsFileManager/PointsFileManager.h
@@ -3,6 +3,8 @@
 
 #include <fstream>
 #include <string>
+#include "../Point2D/Point2D.h"
+#include "../Point3D/Point3D.h"
 
 class PointsFileManager {
     private:
@@ -15,6 +17,11 @@ class PointsFileManager {
         int getSize();
         std::string getFilename();
         std::string getPath();
+        void createPoints(Point2D *pointsArray, int size);
+        void createPoints(Point3D *pointsArray, int size);
+        void clearFlag();
+        Point2D create2dPoint(std::string line);
+        Point3D create3dPoint(std::string line);
 };
 
 #endif
