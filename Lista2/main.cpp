58c1d57ced1540d608a80eab14566bb0af9eb2c5
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-11-07 00:51:05 +0100

    Finished first part of project

--- Lista2/main.cpp
+++ Lista2/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include "classes/PointsFileManager/PointsFileManager.h"
 #include "classes/Point2D/Point2D.h"
 
 int main() {
     const std::string DATA_FILENAME = "data/lamana2D0.csv";
-    Point2D *points;
     PointsFileManager *pfm = new PointsFileManager(DATA_FILENAME);
+    const int numberOfPoints = pfm->getSize();
+    Point2D *points = new Point2D[numberOfPoints];
+    pfm->createPoints(points, numberOfPoints);
 
-    std::cout << pfm->getPath() << std::endl;
+    // Check whether the points belong to the line equation
+    for (int i = 2; i < numberOfPoints; i++) {
+        if (Point2D::isLineEquationSatisfied(points[0], points[1], points[i])) {
+            std::cout << "---------- OK" << std::endl;
+        } else {
+            std::cout << "---------- FAIL" << std::endl;
+        }
+
+        points[i].printPoint();
+        std::cout << "-------------\n" << std::endl;
+    }
+
+    delete pfm, points;
 
     return 0;
 }
